777fe78af6dfaa1e0b71ecb44ff91badbba93b5e
mayalunaa <102543472+mayalunaa@users.noreply.github.com>
2023-01-25 23:37:25 -0800

    Made a cute floating arrow indicator

--- GDScript/LetterFade.gd
+++ GDScript/LetterFade.gd
@@ -2,13 +2,17 @@ extends AnimationPlayer
 
 var textBox
 var textBoxFadeAway = false
+var counter = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	textBox = get_parent().get_node("TextBox")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(delta):
+	counter = counter + 0.1
+	self.get_node("Arrow").position.y = 348 + (10 * sin(counter))
+	
 	if textBoxFadeAway:
 		textBox.modulate.a = max(textBox.modulate.a - 0.025, 0)
 		if textBox.modulate.a == 0:
