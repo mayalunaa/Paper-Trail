7ad3f2b41c6bb9fc3062c414bb2df98f34c105db
mayalunaa <102543472+mayalunaa@users.noreply.github.com>
2023-02-07 21:59:49 -0800

    Fixed a known bug with the dragging mechanic, if dropped on objects they now react by changing frame

--- Scenes/TableLetter.gd
+++ Scenes/TableLetter.gd
@@ -4,11 +4,14 @@ var timer : float
 var og_mouse_pos : Vector2
 var trans : Sprite
 var onLetter : bool
-var moveLetter
+var moveLetter : bool
+var root : Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	trans = get_parent().get_parent().get_node("TransSprite")
+	root = get_tree().root.get_child(0)
+	print(root.name)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -23,13 +26,13 @@ func _process(delta):
 			timer = 0
 	elif onLetter:
 		if Input.is_mouse_button_pressed(BUTTON_LEFT):
-			if timer < 0.5: print(timer)
+			if timer < 0.25: print(timer)
 			timer += delta
 		else:
 			timer = 0
 			trans.visible = false
 
-	if timer >= 0.5: moveLetter()
+	if timer >= 0.25: moveLetter()
 
 func moveLetter():
 	trans.visible = true
@@ -44,10 +47,58 @@ func moveLetter():
 		get_parent().offset = diff_pos
 		self.position = self.position + diff_pos
 	else:
-		moveLetter = false
-		get_parent().offset = Vector2()
-		self.position = Vector2(-9,96)
+		resetPosition()
 
+func resetPosition(pos = Vector2(-9,96)):
+	moveLetter = false
+	get_parent().offset = Vector2()
+	self.position = pos
+	if pos != Vector2(-9,96): trans.position = pos
 
-func _on_TableLetter_input_event(viewport, event, shape_idx):
+func _on_TableLetter_input_event(_viewport, _event, _shape_idx):
 	if not onLetter: onLetter = true
+
+func _on_Board_input_event(viewport, event, shape_idx):
+	if moveLetter and not Input.is_mouse_button_pressed(BUTTON_LEFT):
+		resetPosition()
+		var board = root.get_node("Board/BoardSprite")
+		if board.frame < 5:
+			board.frame = board.frame + 1
+		else:
+			board.frame = 0
+
+func _on_Trash_input_event(viewport, event, shape_idx):
+	if moveLetter and not Input.is_mouse_button_pressed(BUTTON_LEFT):
+		resetPosition()
+		var trash = root.get_node("Trash/TrashSprite")
+		if trash.frame < 5:
+			trash.frame = trash.frame + 1
+		else:
+			trash.frame = 0
+
+func _on_Folder_input_event(viewport, event, shape_idx):
+	if moveLetter and not Input.is_mouse_button_pressed(BUTTON_LEFT):
+		resetPosition()
+		var folder = root.get_node("Folder/FolderSprite")
+		if folder.frame < 5:
+			folder.frame = folder.frame + 1
+		else:
+			folder.frame = 0
+
+func _on_Letter_input_event(viewport, event, shape_idx):
+	if moveLetter and not Input.is_mouse_button_pressed(BUTTON_LEFT):
+		resetPosition()
+		var letter = root.get_node("Letter/LetterSprite")
+		if letter.frame < 5:
+			letter.frame = letter.frame + 1
+		else:
+			letter.frame = 0
+
+func _on_Box_input_event(viewport, event, shape_idx):
+	if moveLetter and not Input.is_mouse_button_pressed(BUTTON_LEFT):
+		resetPosition()
+		var box = root.get_node("Box/BoxSprite")
+		if box.frame < 5:
+			box.frame = box.frame + 1
+		else:
+			box.frame = 0
